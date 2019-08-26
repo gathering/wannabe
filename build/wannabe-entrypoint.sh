f834bcd782d8c6132f54c659cd41ece19e774a92
Nicolai R. Tellefsen <niccofyren@gmail.com>
2019-08-27 01:05:30 +0200

    Add docker compose dev env for php 5 and 7 versions

---
+++ build/wannabe-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /var/www/html
+mkdir -p app/tmp
+mkdir -p app/tmp/sessions
+mkdir -p app/tmp/logs
+mkdir -p app/tmp/tests
+mkdir -p app/tmp/cache app/tmp/cache/{persistent,models,assets}
+chmod -R 777 app/tmp
+mkdir -p app/webroot/files
+chmod 777 app/webroot/files
+chown -R www-data:www-data ./
+
+exec "$@"
