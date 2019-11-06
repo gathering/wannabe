64f6dc38ed5c66c181a773a21513068884f430a8
Nicolai R. Tellefsen <niccofyren@gmail.com>
2019-11-06 18:20:13 +0100

    Fix additional AuthController issue

--- app/Controller/TaskController.php
+++ app/Controller/TaskController.php
@@ -5,7 +5,7 @@ class TaskController extends AppController {
         if(isset($this->TaskHandler->task['Task']) && $task == $this->TaskHandler->task['Task']['id']) {
             if($this->TaskHandler->task['Task']['skip_button']) {
                 $this->UserTask->query('UPDATE wb4_user_tasks SET completed = 1 WHERE user_id='.$this->Wannabe->user['User']['id'].' AND task_id='.$this->TaskHandler->task['Task']['id'].' AND event_id='.$this->Wannabe->event->id);
-                $this->Auth->reloadUserLogin($this->Wannabe->user['User']['id']);
+                $this->Auth->reloadUserLogin($this->Wannabe->user['User']['id'], $controller);
             }
         }
         if(CakeSession::check('taskRedir')){
