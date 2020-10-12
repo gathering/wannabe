c271c2968c55916e1773e3ac4ea3e589902f0187
Nicolai R. Tellefsen <niccofyren@gmail.com>
2020-10-13 01:27:46 +0200

    Enable editing of FrontNews items regardless of user language

--- app/Controller/FrontNewsManagerController.php
+++ app/Controller/FrontNewsManagerController.php
@@ -6,6 +6,7 @@
 class FrontNewsManagerController extends AppController {
 
 	public $uses = array('FrontNews');
+
 	public function index() {
 		$this->set('frontNews', $this->FrontNews->find('all'));
 		$this->set('title_for_layout', __("Front news"));
@@ -18,7 +19,6 @@ class FrontNewsManagerController extends AppController {
 	}
 	public function add() {
 		if($this->request->is('post')) {
-			$this->FrontNews->locale = $this->Wannabe->lang;
 			if($this->FrontNews->save($this->request->data)) {
 				Cache::delete('news'.$this->Wannabe->lang);
 				$this->Flash->success(__("The news has been saved."));
@@ -31,8 +31,8 @@ class FrontNewsManagerController extends AppController {
 		$this->FrontNews->id = $id;
 		if($this->request->is('get')) {
 			$this->request->data = $this->FrontNews->read();
-		} else {
-			$this->Post->locale = $this->Wannabe->lang;
+		} elseif ($this->request->is('post')) {
+			$this->FrontNews->locale = $this->request->data['locale'];
 			if($this->FrontNews->save($this->request->data)) {
 				Cache::delete('news'.$this->Wannabe->lang);
 				$this->Flash->success(__("The news has been saved."));
@@ -42,4 +42,23 @@ class FrontNewsManagerController extends AppController {
 		$this->set('title_for_layout', __("Edit %s", $this->request->data['FrontNews']['name']));
 	}
 
+	// Automatically set locale for FrontNews and relevant page variables
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$reqLocale = '';
+
+		// Get from query argument on get requests
+		if ($this->request->is('get') && isset($this->request->query['locale'])) {
+			$reqLocale = $this->request->query['locale'];
+		}
+		// Get from form data on post requests
+		if ($this->request->is('post') && isset($this->request->data['locale'])) {
+			$reqLocale = $this->request->data['locale'];
+		}
+
+		// Fall back to user/system defaults
+		$locale = empty($reqLocale) ? $this->Wannabe->lang : $reqLocale;
+		$this->FrontNews->locale = $locale;
+		$this->set('desc_for_layout', $locale === 'nob' ? __("Norwegian translation") : __("English translation"));
+	}
 }
