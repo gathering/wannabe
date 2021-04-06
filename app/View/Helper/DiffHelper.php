0e25cb2ddac40af1722b90e6c470f14acdbf0fa6
Nicolai R. Tellefsen <niccofyren@gmail.com>
2021-04-07 01:13:07 +0200

    Add some filtering to diffs

--- app/View/Helper/DiffHelper.php
+++ app/View/Helper/DiffHelper.php
@@ -6,7 +6,7 @@ use SebastianBergmann\Diff\Output\UnifiedDiffOutputBuilder;
 class DiffHelper extends Helper {
 	public function contents($a, $b) {
 		$differ = new Differ('', true);
-		$raw_diff = $differ->diff($b, $a);
+		$raw_diff = htmlspecialchars($differ->diff($b, $a), ENT_QUOTES, 'UTF-8');
 		return str_replace("\n", "</br>", $raw_diff);
 	}
 }
